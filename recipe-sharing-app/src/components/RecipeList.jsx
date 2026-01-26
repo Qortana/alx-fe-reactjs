3607e9b0f14ffbb7d74bd04975111b9cf957841c
Qortana <panashemoyo10@gmail.com>
2026-01-27 01:49:12 +0200

    add search and filtering with Zustand

--- recipe-sharing-app/src/components/RecipeList.jsx
+++ recipe-sharing-app/src/components/RecipeList.jsx
@@ -2,11 +2,13 @@ import { useRecipeStore } from './recipeStore';
 import { Link } from 'react-router-dom';
 
 const RecipeList = () => {
-  const recipes = useRecipeStore((state) => state.recipes);
+  const filteredRecipes = useRecipeStore(
+    (state) => state.filteredRecipes
+  );
 
   return (
     <div>
-      {recipes.map((recipe) => (
+      {filteredRecipes.map((recipe) => (
         <div key={recipe.id}>
           <h3>
             <Link to={`/recipes/${recipe.id}`}>{recipe.title}</Link>
