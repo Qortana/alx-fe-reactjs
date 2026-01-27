5d8b9a344c98bf3ccdd7eda97c0d1a8664e9c378
Qortana <panashemoyo10@gmail.com>
2026-01-27 16:45:18 +0200

    Add UserContext.js file

--- alx-react-app-props/src/components/UserContext.jsx
+++
@@ -1,8 +0,0 @@
-import { createContext } from 'react';
-
-// Create a context for the counter
-// Initial value is 0
-const UserContext = createContext(0);
-
-// Export it so other components can use it
-export default UserContext;
