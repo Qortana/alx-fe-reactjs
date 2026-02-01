a8f6fcf674704861973cb4d8f6ef5d12a2588eb0
Qortana <panashemoyo10@gmail.com>
2026-02-01 15:02:36 +0200

    Manually configure Tailwind CSS for advanced search UI

--- github-user-search/src/App.jsx
+++ github-user-search/src/App.jsx
@@ -4,7 +4,9 @@ import Search from "./components/Search";
 function App() {
   return (
     <div style={{ padding: "2rem" }}>
-      <h1>GitHub User Search</h1>
+      <h1 className="text-red-500 text-3xl font-bold">
+        GitHub User Search
+      </h1>
       <p>Project setup complete 🚀</p>
       <Search/>
     </div>
