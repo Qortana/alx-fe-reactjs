a9db1af3a5eb21e57565e41d705e8920f6588f32
Qortana <panashemoyo10@gmail.com>
2026-02-01 14:04:10 +0200

    Set up GitHub User Search React project

---
+++ github-user-search/src/App.jsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div style={{ padding: "2rem" }}>
+      <h1>GitHub User Search</h1>
+      <p>Project setup complete 🚀</p>
+    </div>
+  );
+}
+
+export default App;
