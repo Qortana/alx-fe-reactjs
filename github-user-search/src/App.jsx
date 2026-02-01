33832c7671924d8ce465198eeede5381e85c0e45
Qortana <panashemoyo10@gmail.com>
2026-02-01 14:25:36 +0200

    Intergrate GitHub API and add user search feature

--- github-user-search/src/App.jsx
+++ github-user-search/src/App.jsx
@@ -1,9 +1,12 @@
 import React from "react";
+import Search from "./components/Search";
+
 function App() {
   return (
     <div style={{ padding: "2rem" }}>
       <h1>GitHub User Search</h1>
       <p>Project setup complete 🚀</p>
+      <Search/>
     </div>
   );
 }
