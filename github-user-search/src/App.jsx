72381e78ef0eae7ba2643295e249d766876145a0
Qortana <panashemoyo10@gmail.com>
2026-02-01 16:26:54 +0200

    error correction

--- github-user-search/src/App.jsx
+++ github-user-search/src/App.jsx
@@ -3,8 +3,8 @@ import Search from "./components/Search";
 
 function App() {
   return (
-    <div style={{ padding: "2rem" }}>
-      <h1 className="text-red-500 text-3xl font-bold">
+   <div className="flex justify-center items-center h-screen bg-gray-100">
+      <h1 className="text-red-500 text-5xl font-bold">
         GitHub User Search
       </h1>
       <p>Project setup complete 🚀</p>
