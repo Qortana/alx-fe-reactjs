702953808f5a2484cec21019bea1389eca7f70a8
Qortana <panashemoyo10@gmail.com>
2026-02-14 11:53:18 +0200

    Add sm:p-4 for responsive container padding

--- tailwind-react-integration/src/components/UserProfile.jsx
+++ tailwind-react-integration/src/components/UserProfile.jsx
@@ -1,8 +1,6 @@
-// src/components/UserProfile.jsx
-
 function UserProfile() {
   return (
-    <div className="user-profile bg-gray-100 p-4 md:p-8 max-w-xs md:max-w-sm mx-auto my-20 rounded-lg shadow-lg">
+    <div className="user-profile bg-gray-100 p-4 sm:p-4 md:p-8 max-w-xs md:max-w-sm mx-auto my-20 rounded-lg shadow-lg">
       
       <img
         src="https://via.placeholder.com/150"
@@ -10,7 +8,7 @@ function UserProfile() {
         className="rounded-full w-24 h-24 md:w-36 md:h-36 mx-auto"
       />
 
-      <h1 className="text-lg md:text-xl text-blue-800 my-4 text-center">
+      <h1 className="text-blue-800 text-lg md:text-xl my-4 text-center">
         John Doe
       </h1>
 
