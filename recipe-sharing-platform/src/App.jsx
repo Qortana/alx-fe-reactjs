c20b171be1cfa605ad534fd21f1794829e6f60cd
Qortana <panashemoyo10@gmail.com>
2026-02-14 16:13:21 +0200

    Add HomePage with responsive recipe cards using Tailwind

--- recipe-sharing-platform/src/App.jsx
+++ recipe-sharing-platform/src/App.jsx
@@ -1,9 +1,11 @@
+import HomePage from './components/HomePage';
+
 function App() {
   return (
-    <h1 className="text-blue-500 text-3xl font-bold text-center mt-10">
-      Recipe Sharing Platform
-    </h1>
-  )
+    <div>
+      <HomePage />
+    </div>
+  );
 }
 
-export default App
+export default App;
